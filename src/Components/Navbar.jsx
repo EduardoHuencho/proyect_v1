b3f9e3411ac50f6d344c7d585b592183b576612c
obiwancho <inversecrow@gmail.com>
2026-06-17 17:36:37 -0400

    vista home tutor y vista ninos

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,8 +4,9 @@ import { Link } from 'react-router'
 function Navbar() {
   return (
     <nav className="bg-white border-b p-4 flex gap-4">
-      <Link to="/" className="text-blue-600 hover:underline">Page 1</Link>
-      <Link to="/page2" className="text-blue-600 hover:underline">Page 2</Link>
+      <Link to="/" className="text-blue-600 hover:underline">Log</Link>
+      <Link to="/page1" className="text-blue-600 hover:underline">Tutor</Link>
+      <Link to="/page2" className="text-blue-600 hover:underline">Niños</Link>
       <Link to="/pictogramas" className="text-blue-600 hover:underline">Pictogramas</Link>
     </nav>
   )
